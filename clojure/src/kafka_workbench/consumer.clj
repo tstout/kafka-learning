cd1e9c399135adaf89d122cf3d546d6b152badf6
Todd Stout <todd.tstout@gmail.com>
2020-11-01 10:23:08 -0600

    minor cleanup

--- clojure/src/kafka_workbench/consumer.clj
+++ clojure/src/kafka_workbench/consumer.clj
@@ -1,7 +1,6 @@
 (ns kafka-workbench.consumer
   (:require [kafka-workbench.conf :refer [consumer-config]]
             [jackdaw.client :as jc]
-            [jackdaw.client.log :as jl]
             [taoensso.timbre :as log])
   (:import [org.apache.kafka.common.errors WakeupException]))
 
@@ -16,7 +15,10 @@
    jc/consumer
    (jc/subscribe [{:topic-name topic}])))
 
-(defn sub-poll [consumer]
+(defn sub-poll 
+  "Creates a function which continuously polls a kafka topic.
+   Intended to be used as a Runnable for a Thread instance."
+  [consumer]
   (fn []
     (log/info "Started Consumer Thread")
     (try
@@ -27,7 +29,11 @@
 
 
 (defn subscriber
-  "Create a consumer which polls a topic for messages."
+  "Create a consumer which polls a topic for messages.
+   The functions returned accpets three operations:
+   :start - start a polling thread
+   :stop - terminates the polling thread
+   :consumer - returns a KafkaConsumer object"
   [topic group]
   (let [consumer (sub topic group)
         thr (Thread. (sub-poll consumer))
@@ -38,13 +44,11 @@
 
 (comment
 
-  (def consumer (sub "foo"))
-
+  (def consumer (subscriber "foo" "foo.bar"))
+  
   (consumer :start)
   (consumer :stop)
-
-  (bean (consumer :consumer))
-
+  (consumer :consumer)
 
   (.subscription consumer)
 
@@ -54,8 +58,6 @@
 
   ;; show offset
   (-> :consumer consumer jc/position-all)
-
-  (class consumer)
 
   consumer
   ;;
